7ce93551e0312bcf9480a1d2346762cc0d59d0ec
ArthurDelaunay <delaunayfredericarthur@gmail.com>
2022-07-19 14:21:37 +0200

    exercices 1-3 done

---
+++ index.js
@@ -0,0 +1,58 @@
+// 1 - Hello world
+//
+// const writeSentence = () => {
+//     return new Promise ( (resolve, reject) => {
+//         setTimeout ( () => {
+//             resolve("hello world")
+//         }, 2000)
+//        
+//     })
+// }
+// const waitForResult = async () => {
+//     const result = await writeSentence()
+//     console.log(result)
+// }
+// waitForResult()
+
+// 2 - Il fait froid
+//
+// const isWarm = () => {
+//     return new Promise ( (resolve, reject) => {
+//         const temperature = 15
+//         setTimeout ( () => {
+//             temperature <= 5 ? resolve("Il fait froid") : resolve("Il fait chaud")
+//         }, 4000)    
+//     })
+// }
+// const waitForResult = async () => {
+//     const result = await isWarm()
+//     console.log(result)
+// }
+// waitForResult()
+
+// 3 - Ménage
+//
+const laundry = () => {
+    return new Promise ( (resolve, reject) => {
+        console.log("Je commence la lessive")
+        setTimeout ( () => {
+            resolve("j'ai fini la lessive")
+        }, 3000)
+    })
+}
+const cleanDishes = () => {
+    return new Promise ( (resolve, reject) => {
+        console.log("Je commence à faire la vaisselle")
+        setTimeout( () => {
+            resolve("j'ai fini la vaiselle")
+        }, 1500)
+    })
+}
+const waitForResult = async () => {
+    let result = await laundry()
+    console.log(result)
+    result = await cleanDishes()
+    console.log(result)
+    console.log("j'ai fini de faire le ménage")
+}
+waitForResult()
